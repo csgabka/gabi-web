7306239ed2b8db9a965e27f6af75431edd68f886
Gabi <you@example.com>
2020-04-14 15:23:04 +0100

    fade-in

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+function pageScroll(value) {
+	document.getElementById(value).scrollIntoView({behavior: "smooth"});
+}
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+	sliderImages.forEach(sliderImage => {
+		// halfway through the image
+		const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+		// bottom of the image
+		const imageBottom = sliderImage.offsetTop + sliderImage.height;
+		const isHalfShown = slideInAt > sliderImage.offsetTop;
+		const isNotScrolledPast = window.scrollY < imageBottom;
+		if (isHalfShown && isNotScrolledPast) {
+			sliderImage.classList.add('active');
+		} else {
+			sliderImage.classList.remove('active');
+		}
+
+		console.log(slideInAt);
+	});
+}
+
+window.addEventListener('scroll', checkSlide);
